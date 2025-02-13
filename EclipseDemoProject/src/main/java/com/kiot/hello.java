07a1e53a58e9f77337dab46fc63761fd9572147a
Divraj-M <2k21cse022@kiot.ac.in>
2025-02-13 10:00:24 +0530

    Created the KIOT hello file

---
+++ EclipseDemoProject/src/main/java/com/kiot/hello.java
@@ -0,0 +1,12 @@
+package com.kiot;
+import java.util.*;
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		
+
+	}
+
+}
