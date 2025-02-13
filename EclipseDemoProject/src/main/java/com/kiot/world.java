fea95ed34cea48d5523ef7621223b2cbc384c158
GOWRISANKARHARI <2k21ece032@kiot.ac.in>
2025-02-13 11:06:47 +0530

    NEW

--- EclipseDemoProject/src/main/java/com/kiot/world.java
+++ EclipseDemoProject/src/main/java/com/kiot/world.java
@@ -5,6 +5,7 @@ public class world {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("hello this is divi + raj");
+			System.out.println("THI SIS BABABABABABBA");
 	}
 
 }
